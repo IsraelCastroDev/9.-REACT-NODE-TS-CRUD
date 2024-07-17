6890d0f0486bc1cd96fae3301d4fc7a0a88765d4
Isreal Castro <israelcastro.dev@gmail.com>
2024-07-17 15:32:58 -0500

    Endpoint para obtener el producto por su id

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,11 +1,18 @@
 import { Router } from "express";
-import { body } from "express-validator";
-import { createProduct, getProducts } from "./handlers/product";
+import { body, param } from "express-validator";
+import { createProduct, getProductById, getProducts } from "./handlers/product";
 import { handleInputErrors } from "./middlewares";
 
 const router = Router();
 
 router.get("/", getProducts);
+
+router.get(
+  "/:id",
+  param("id").isInt().withMessage("ID no válido"),
+  handleInputErrors,
+  getProductById
+);
 
 router.post(
   "/",
