41472982b70bd748839216149526377182b35c92
Isreal Castro <israelcastro.dev@gmail.com>
2024-07-17 14:02:27 -0500

    Ejecutando la creacion de modelos en la DB

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,10 +1,12 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from "sequelize-typescript";
 import dotenv from "dotenv";
 
 // cargamos las variables de entorno
 dotenv.config();
 
 // configuramos la base de datos
-const db = new Sequelize(process.env.DATABASE_URL!);
+const db = new Sequelize(process.env.DATABASE_URL!, {
+  models: [__dirname + "/../models/**/*.ts"],
+});
 
 export default db;
