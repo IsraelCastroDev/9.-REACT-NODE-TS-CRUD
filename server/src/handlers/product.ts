f7735326bffa07e49bcdc87f938b72069bf7afc8
Isreal Castro <israelcastro.dev@gmail.com>
2024-07-17 15:46:35 -0500

    Actualizar producto

--- server/src/handlers/product.ts
+++ server/src/handlers/product.ts
@@ -50,3 +50,21 @@ export const createProduct = async (req: Request, res: Response) => {
     console.log(error);
   }
 };
+
+export const updateProduct = async (req: Request, res: Response) => {
+  try {
+    const product = await Product.findByPk(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({
+        error: "Producto no encontrado",
+      });
+    }
+
+    // actualizar
+    await product.update(req.body);
+    await product.save();
+  } catch (error) {
+    console.log(error);
+  }
+};
--- server/src/router.ts
+++ server/src/router.ts
@@ -1,6 +1,11 @@
 import { Router } from "express";
 import { body, param } from "express-validator";
-import { createProduct, getProductById, getProducts } from "./handlers/product";
+import {
+  createProduct,
+  getProductById,
+  getProducts,
+  updateProduct,
+} from "./handlers/product";
 import { handleInputErrors } from "./middlewares";
 
 const router = Router();
@@ -29,9 +34,21 @@ router.post(
   createProduct
 );
 
-router.put("/", (req, res) => {
-  res.json("Desde put");
-});
+router.put(
+  "/:id",
+  param("id").isInt().withMessage("ID no válido"),
+  body("name").notEmpty().withMessage("El nombre es obligatorio"),
+  body("price")
+    .isNumeric()
+    .withMessage("Valor no válido")
+    .notEmpty()
+    .withMessage("El precio es obligatorio")
+    .custom((value) => value > 0)
+    .withMessage("Precio no válido"),
+  body("availability").isBoolean().withMessage("Valor inválido"),
+  handleInputErrors,
+  updateProduct
+);
 
 router.patch("/", (req, res) => {
   res.json("Desde patch");
