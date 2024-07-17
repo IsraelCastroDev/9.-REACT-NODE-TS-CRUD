94c0faddade1eb6493be57c46040b5e5f58e0b6b
Isreal Castro <israelcastro.dev@gmail.com>
2024-07-17 15:03:17 -0500

    Validando campos

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,4 +1,7 @@
 import { Router } from "express";
+import { body } from "express-validator";
+import { createProduct } from "./handlers/product";
+import { handleInputErrors } from "./middlewares";
 
 const router = Router();
 
@@ -6,9 +9,20 @@ router.get("/", (req, res) => {
   res.json("Desde get");
 });
 
-router.post("/", (req, res) => {
-  res.json("Desde post");
-});
+router.post(
+  "/",
+  // validación
+  body("name").notEmpty().withMessage("El nombre es obligatorio"),
+  body("price")
+    .isNumeric()
+    .withMessage("Valor no válido")
+    .notEmpty()
+    .withMessage("El precio es obligatorio")
+    .custom((value) => value > 0)
+    .withMessage("Precio no válido"),
+  handleInputErrors,
+  createProduct
+);
 
 router.put("/", (req, res) => {
   res.json("Desde put");
