e6880d6df903289857c74ea2663786682f680c16
Isreal Castro <israelcastro.dev@gmail.com>
2024-07-18 13:39:46 -0500

    Sacando try catch innecesario para mejorar code coverage

--- server/src/handlers/product.ts
+++ server/src/handlers/product.ts
@@ -2,107 +2,83 @@ import { Request, Response } from "express";
 import Product from "../models/Product.model";
 
 export const getProducts = async (req: Request, res: Response) => {
-  try {
-    const products = await Product.findAll({
-      order: [["id", "ASC"]],
-    });
+  const products = await Product.findAll({
+    order: [["id", "ASC"]],
+  });
 
-    if (!products.length) {
-      return res.status(404).json({
-        error: "No hay productos",
-      });
-    }
-
-    res.json({
-      data: products,
+  if (!products.length) {
+    return res.status(404).json({
+      error: "No hay productos",
     });
-  } catch (error) {
-    console.log(error);
   }
+
+  res.json({
+    data: products,
+  });
 };
 
 export const getProductById = async (req: Request, res: Response) => {
-  try {
-    const product = await Product.findByPk(req.params.id);
+  const product = await Product.findByPk(req.params.id);
 
-    if (!product) {
-      return res.status(404).json({
-        error: "Producto no encontrado",
-      });
-    }
-
-    res.json({
-      data: product,
+  if (!product) {
+    return res.status(404).json({
+      error: "Producto no encontrado",
     });
-  } catch (error) {
-    console.log(error);
   }
+
+  res.json({
+    data: product,
+  });
 };
 
 export const createProduct = async (req: Request, res: Response) => {
-  try {
-    const product = await Product.create(req.body);
+  const product = await Product.create(req.body);
 
-    res.json({
-      data: product,
-    });
-  } catch (error) {
-    console.log(error);
-  }
+  res.status(201).json({
+    data: product,
+  });
 };
 
 export const updateProduct = async (req: Request, res: Response) => {
-  try {
-    const product = await Product.findByPk(req.params.id);
+  const product = await Product.findByPk(req.params.id);
 
-    if (!product) {
-      return res.status(404).json({
-        error: "Producto no encontrado",
-      });
-    }
-
-    // actualizar
-    await product.update(req.body);
-    await product.save();
-  } catch (error) {
-    console.log(error);
+  if (!product) {
+    return res.status(404).json({
+      error: "Producto no encontrado",
+    });
   }
+
+  // actualizar
+  await product.update(req.body);
+  await product.save();
 };
 
 export const updateAvailability = async (req: Request, res: Response) => {
-  try {
-    const product = await Product.findByPk(req.params.id);
+  const product = await Product.findByPk(req.params.id);
 
-    if (!product) {
-      return res.status(404).json({
-        error: "Producto no encontrado",
-      });
-    }
-
-    // actualizar
-    product.availability = !product.dataValues.availability;
-    await product.save();
-  } catch (error) {
-    console.log(error);
+  if (!product) {
+    return res.status(404).json({
+      error: "Producto no encontrado",
+    });
   }
+
+  // actualizar
+  product.availability = !product.dataValues.availability;
+  await product.save();
 };
 
 export const deleteProduct = async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findByPk(id);
+  const { id } = req.params;
+  const product = await Product.findByPk(id);
 
-    if (!product) {
-      return res.status(404).json({
-        error: "Producto no encontrado",
-      });
-    }
-
-    await product.destroy();
-    res.json({
-      data: "Producto eliminado",
+  if (!product) {
+    return res.status(404).json({
+      error: "Producto no encontrado",
     });
-  } catch (error) {
-    console.log(error);
   }
+
+  await product.destroy();
+  res.json({
+    data: "Producto eliminado",
+  });
 };
